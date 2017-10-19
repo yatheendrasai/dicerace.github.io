87715ed2059213e1ce3ed80d0f832963992ca91c
yatheendrasai <yatheendrarockz9@gmail.com>
2017-10-19 17:58:45 +0530

    target raised to 100

--- mygamejs.js
+++ mygamejs.js
@@ -54,7 +54,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     --------------------------------MY WAY*/
     scores[activePlayer]+=roundScore;
     document.getElementById('score-'+activePlayer).textContent=scores[activePlayer];
-    if(scores[activePlayer]>=10){
+    if(scores[activePlayer]>=100){
         document.getElementById('name-'+activePlayer).textContent='WINNER!'
         document.querySelector('.dice').style.display='none';
         //document.querySelector('.player-'+activePlayer+'-panel').classList.add('player-name');
